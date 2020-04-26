aceeb62126d6e2e4cd75439ddf7c232a227518b4
matthew-j <42253617+matthew-j@users.noreply.github.com>
2020-04-26 18:30:22 -0500

    Update inputGenerator.py

--- inputs/inputGenerator.py
+++ inputs/inputGenerator.py
@@ -1,5 +1,6 @@
 import random
 
+## n  is the number of elements to be generated
 n = 200000
 s = ""
 for i in range(n):
@@ -7,6 +8,7 @@ for i in range(n):
 	if i != n-1:
 		s += ", "
 
+## this is the name of the file that will be used for input
 text_file = open("test_200000.txt", "w")
 text_file.write(s)
 text_file.close()
